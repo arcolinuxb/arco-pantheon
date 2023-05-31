f75d362a3bcfd9d9b6c37a7f7460cc74b7199816
arcolinuxz <arcolinuxinfo@gmail.com>
2023-05-31 12:52:08 +0200

    archiso 71

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="pantheon"
 	dmDesktop="pantheon"
 
-	arcolinuxVersion='v23.06.02'
+	arcolinuxVersion='v23.07.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
